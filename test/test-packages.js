73dc8a1a854bf4fd6b571cae9133ef5456b275dc
Cyborg Simon <cyborg.simon@gmail.com>
2015-01-26 00:53:14 +0100

    improved packages template and added test for none

--- test/test-packages.js
+++ test/test-packages.js
@@ -20,6 +20,24 @@ describe('meteor coffee generator', function () {
         }.bind(this));
     });
     
+    // test default packages
+    it('creates app with stylus as CSS preprocessor', function (done) {
+      helpers.mockPrompt(this.app, {
+        'cssPreprocessor': 'stylus'
+      });
+      this.app.run({}, function () {
+        helpers.assertFileContent([
+          ['.meteor/packages', /\nstandard-app-packages\n/],
+          ['.meteor/packages', /\ncoffeescript\n/],
+          ['.meteor/packages', /\njquery\n/],
+          ['.meteor/packages', /\niron:router\n/],
+          ['.meteor/packages', /\nfastclick\n/],
+          ['.meteor/packages', /\nunderscore\n/]
+        ]);
+        done();
+      });
+    });
+    
     // test css preprocessor
     it('creates app with stylus as CSS preprocessor', function (done) {
       helpers.mockPrompt(this.app, {
@@ -57,6 +75,21 @@ describe('meteor coffee generator', function () {
       });
       this.app.run({}, function () {
         helpers.assertFileContent('.meteor/packages', /\nless\nnemo64:bootstrap\n/);
+        done();
+      });
+    });
+    
+    it('creates app with no CSS preprocessor', function (done) {
+      helpers.mockPrompt(this.app, {
+        'cssPreprocessor': 'none'
+      });
+      this.app.run({}, function () {
+        helpers.assertNoFileContent([
+          ['.meteor/packages', /\nstylus\n/],
+          ['.meteor/packages', /\nless\n/],
+          ['.meteor/packages', /\nbootstrap\n/],
+          ['.meteor/packages', /\nless\nnemo64:bootstrap\n/]
+        ]);
         done();
       });
     });
